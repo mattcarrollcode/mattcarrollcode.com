b5cac6a630489d2cd4e3bcba72c67868f640a4a2
Matt Carroll <me@matthewwaynecarroll.com>
2023-01-30 23:03:10 -0800

    WIP2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import './globals.css';
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head />
-            <body className="h-screen bg-gradient-to-b from-green-200 to-green-500">{children}</body>
+            <body className="antialiased mb-40 flex flex-col mx-4 mt-8 bg-gradient-to-r from-gray-900 via-stone-900 to-black">{children}</body>
         </html>
     );
 }
