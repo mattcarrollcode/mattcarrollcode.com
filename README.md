dc38ab6e1e86909031e9a97c4d0d3ee479f494e0
Matt Carroll <me@matthewwaynecarroll.com>
2023-02-03 21:19:56 -0800

    Pretty shippable

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 1. Install nvm: https://github.com/nvm-sh/nvm#installing-and-updating
 1. install node: `nvm install --lts`
 1. instal depedencies: `cd` to this dir, `npm install`
+
+### Making changes
+1. Run the dev server: `npm run dev`
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import {
     TwitterIcon,
     LinkedInIcon,
     ReactIcon,
-} from 'components/icons';
+} from '../components/icons';
 
 export default function Page() {
-    return <>
-        <div className="flex flex-row">
+    return <div className="flex flex-col gap-7">
+        <div className="flex flex-row justify-center gap-7">
             <div className="flex flex-col gap-2">
+                <ProfilePicture />
+            </div>
+            <div className="flex flex-col w-64 gap-2">
                 <h1 className="font-bold text-3xl font-sans-serif text-white">Matt Carroll</h1>
                 <Description />
             </div>
@@ -24,11 +27,15 @@ export default function Page() {
                 <LinkedInIcon />
             </a>
         </div>
-    </>;
+    </div>
 }
 
 function Description() {
     return <p className="text-white">
-        Hi, I'm Matt! I help people build better developer products. Currently, I'm working on <a target="_blank" className="underline hover:text-purple-600" href="https://reactjs.org">React</a>. Previously, I've worked on products like <a target="_blank" className="underline hover:text-purple-600" href="https://developers.google.com/youtube">YouTube</a>, <a target="_blank" className="underline hover:text-purple-600" href="https://fuchsia.dev/">Fuchsia</a>, <a target="_blank" className="underline hover:text-purple-600" href="https://cloud.google.com/products/ai">Google Cloud AI</a>, and the <a target="_blank" className="underline hover:text-purple-600" href="https://developers.google.com/assistant">Google Assistant</a>.
+        Hey, I'm Matt - I help people build better developer products. Right now, I'm working on <a target="_blank" className="underline hover:text-purple-600" href="https://reactjs.org">React</a>. I used to work on products like <a target="_blank" className="underline hover:text-purple-600" href="https://developers.google.com/youtube">YouTube</a>, <a target="_blank" className="underline hover:text-purple-600" href="https://fuchsia.dev/">Fuchsia</a>, <a target="_blank" className="underline hover:text-purple-600" href="https://cloud.google.com/products/ai">Google Cloud AI</a>, and the <a target="_blank" className="underline hover:text-purple-600" href="https://developers.google.com/assistant">Google Assistant</a>.
     </p>
+}
+
+function ProfilePicture() {
+    return <img src="/profile.png" className="rounded-full object-cover h-48 w-48" />
 }
