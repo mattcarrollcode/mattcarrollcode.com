fa8fd5e01ea3c032cefb42b5afc6b88d3c829444
Matt C <mwc@Matts-MBP.localdomain>
2023-04-01 14:44:27 -0700

    add html lang attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html>
+        <html lang="en">
             <head />
             <body className="antialiased mb-40 flex flex-col mx-4 mt-8 bg-gradient-to-r from-gray-900 via-stone-900 to-black text-white bg-black">
                 {children}
