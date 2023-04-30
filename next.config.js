43e460b77ddfed372e5d4ca12cdae0e176ced4bf
Matt Carroll <me@matthewwaynecarroll.com>
2023-04-30 14:43:48 -0700

    Add redirect

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    async redirects() {
+        return [
+          {
+            source: '/rsc-integration',
+            destination: 'https://github.com/reactjs/server-components-demo/commit/37f20f98ce21a4ca3f29d66fd77d08e95c6fd003',
+            permanent: true,
+          },
+        ]
+      },
 };
 
 module.exports = withContentlayer(nextConfig);
